881977acb8c1dd0719076524bbbc383ef2e081dc
kkamara <itchykilljoy@protonmail.com>
2020-01-01 17:22:53 +0000

    added close button for session flash alerts

--- resources/views/layouts/flashes.blade.php
+++ resources/views/layouts/flashes.blade.php
@@ -1,17 +1,26 @@
 @if(session('flashSuccess'))
-    <div class="alert alert-success text-center">
-        <div class="panel-heading">{{ session('flashSuccess') }}</div>
+    <div class="alert alert-success" role="alert">
+        {{ session('flashSuccess') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if(session('flashWarning'))
-    <div class="alert alert-warning text-center">
-        <div class="panel-heading">{{ session('flashWarning') }}</div>
+    <div class="alert alert-warning" role="alert">
+        {{ session('flashWarning') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if(session('flashDanger'))
-    <div class="alert alert-danger text-center">
-        <div class="panel-heading">{{ session('flashDanger') }}</div>
+    <div class="alert alert-danger" role="alert">
+        {{ session('flashDanger') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
